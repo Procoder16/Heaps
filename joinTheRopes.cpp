8d500c2c129e656fdc92dbb618a2b805cc081345
Procoder16 <smsoumik1608@gmail.com>
2021-07-15 13:47:28 +0530

    added the algo comment

--- joinTheRopes.cpp
+++ joinTheRopes.cpp
@@ -6,6 +6,13 @@ using namespace std;
     The cost of joining two ropes of different sizes A and B is (A+B). Find the minimum cost to join all the ropes together.
 */
 
+/*
+    So, the algo to be followed here is:
+        >> Pick the 2 smallest ropes and join them and add to the cost. Also remove these two ropes.
+        >> Add the newly formed rope to the min heap.
+        >> Repeat until the number of ropes is 1
+*/
+
 int join_ropes(int ropes[], int n){
 
     priority_queue<int, vector<int>, greater<int>> pq(ropes, ropes+n);
